1ee68700ca7593161a3ae6e6f8a00880a439b0aa
zachary chapman <zach.chapman24@gmail.com>
2024-02-16 12:13:45 -0500

    removed user and password

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ const inquirer = require("inquirer");
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
-  user: "root",
-  password: "1725",
+  //add user "root"
+  user: "",
+  //add password 
+  password: "",
   database: "employeesDB",
 });
 
